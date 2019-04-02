2e22e4df54e8cebc272337f68be3fec366b47423
Samuel K. Gutierrez <samuel@lanl.gov>
2019-04-02 14:17:13 -0600

    More copyright updates.

--- src/mmu_util.h
+++ src/mmu_util.h
@@ -1,19 +1,13 @@
-/**
- * Copyright (c) 2010-2012 Los Alamos National Security, LLC.
+/*
+ * Copyright (c) 2010-2019 Triad National Security, LLC
  *                         All rights reserved.
  *
- * This program was prepared by Los Alamos National Security, LLC at Los Alamos
- * National Laboratory (LANL) under contract No. DE-AC52-06NA25396 with the U.S.
- * Department of Energy (DOE). All rights in the program are reserved by the DOE
- * and Los Alamos National Security, LLC. Permission is granted to the public to
- * copy and use this software without charge, provided that this Notice and any
- * statement of authorship are reproduced on all copies. Neither the U.S.
- * Government nor LANS makes any warranty, express or implied, or assumes any
- * liability or responsibility for the use of this software.
+ * This file is part of the mpimemu project. See the LICENSE file at the
+ * top-level directory of this distribution.
  */
 
 #ifndef MMU_UTIL_INCLUDED
-#define MMU_UTIL_INCLUDED 
+#define MMU_UTIL_INCLUDED
 
 #include "mmu_constants.h"
 
