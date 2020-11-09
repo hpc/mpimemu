70df07eb6e803d092c43d00ee0ada3425a470d6b
Samuel K. Gutierrez <samuel@lanl.gov>
2020-11-09 12:55:13 -0700

    Fix Copyright.

--- src/mmu_mpi.c
+++ src/mmu_mpi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2019 Triad National Security, LLC
+ * Copyright (c) 2010-2020 Triad National Security, LLC
  *                         All rights reserved.
  *
  * This file is part of the mpimemu project. See the LICENSE file at the
